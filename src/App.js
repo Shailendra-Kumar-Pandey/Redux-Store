e2cc43ccc2da351b7ddbe475b40e94738113dd09
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-04 12:30:08 +0530

    add state & action-creates, reduces

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './component/Navbar';
+import Shop from './component/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <h1>this a app</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Shop/>
+    </>
   );
 }
 
