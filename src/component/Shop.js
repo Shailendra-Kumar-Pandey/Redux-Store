cb79ba1478cc6eb71333ceb5c724b0592ddd84f6
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-04 12:19:52 +0530

    add bootstrap & component

---
+++ src/component/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div className='container'>
+
+      <h2>Buy Addidas Shoes 50 Rs</h2>
+      
+      <button className='btn btn-success mx-2'>-</button>
+      Add to card
+      <button className='btn btn-success mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
