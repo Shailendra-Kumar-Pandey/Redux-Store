83a4c8f60d860f500431b5ab4e4b48e52f111059
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-05 12:03:17 +0530

    use  bindActionCreators

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-// import { blindActionCreators } from "redux";
+import { bindActionCreators } from "redux";
 import {actionCreators} from '../state/index';
+
 
 const Shop = () => {
   const dispatch= useDispatch();
+  const action = bindActionCreators(actionCreators, dispatch)
   return (
     <div className='container'>
 
       <h2>Buy Addidas Shoes 50 Rs</h2>
       
-      <button className='btn btn-success mx-2' onClick={()=>{dispatch(actionCreators.depositMoney(200))}}> + </button>
+      {/* <button className='btn btn-success mx-2' onClick={()=>{dispatch(actionCreators.depositMoney(200))}}> + </button>
       Add to card
-      <button className='btn btn-success mx-2'onClick={()=>{dispatch(actionCreators.withdrawtMoney(200))}}> - </button>
+      <button className='btn btn-success mx-2'onClick={()=>{dispatch(actionCreators.withdrawtMoney(200))}}> - </button> */}
+    
+      <button className='btn btn-success mx-2' onClick={()=>{action.depositMoney(200)}}> + </button>
+      Add to card
+      <button className='btn btn-success mx-2'onClick={()=>{action.withdrawtMoney(200)}}> - </button>
+    
     </div>
   )
 }
